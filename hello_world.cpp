d94ae8c1365fb7af98ac2a9e1b6593f3be45b8a6
ph1l1a <philia@tutamail.com>
2024-08-14 22:02:07 +0300

    translate comments

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,9 @@
 
 int main() { 
 
-	std::string name; // вводим имя пользователя
+	std::string name; // enter user name
 	std::cin >> name;
 
-	std::cout << "Hello world from " << name << std::endl; // выводим имя пользователя
-	return 0; // завершаем программу
+	std::cout << "Hello world from " << name << std::endl; // display user name
+	return 0; // terminate
 }
